a30cf8bb93dacc85f7e027080fa8a1286c08fab4
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-24 19:10:07 +0700

    Thuan commit: insert image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx";
-import Food from "./food.jsx";
+import Card from "./Card"
 
 function App() {
-
   return(
     <>
-    <Header/>
-    <Footer/>
-    <Food/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
     </>
+
   );
 }
 
