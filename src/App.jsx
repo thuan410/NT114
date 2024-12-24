565cfd1f788ee0a837bca2357f13306a00435a7a
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-25 01:02:42 +0700

    Thuan commit: create class

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import Button from "./Button"
+
+import Student from "./Student";
 
 function App() {
-  return(<Button></Button>);
+  return(
+  <>
+  <Student name = "Thuan Tran" age = {30} isStudent={false}></Student>
+  <Student name = "Tran Thuan" age = {20} isStudent={true}></Student>
+  <Student name = "Thuan"></Student>
+  </>
+  );
 }
 
 export default App
