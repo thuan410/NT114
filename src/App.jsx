f9542d1d34fd6bfed4ad31f032e966c500e2651a
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-12-25 09:06:57 +0700

    Thuan commit: create lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
+import List from "./List";
 
 
-import UserGreeting from "./UserGreeting";
-import Student from "./Student";
+
 
 function App() {
-  return(
-  <>
-  <UserGreeting isLoggedIn = {true} username = "Thuan"/>
-  <Student name = "Thuan Tran" age = {30} isStudent={false}></Student>
-  <Student name = "Tran Thuan" age = {20} isStudent={true}></Student>
-  <Student name = "Thuan"></Student>
-  </>
-  );
+  return(<List/>);
 }
 
 export default App
